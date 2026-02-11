965987dd67659d1eaa2070ade7460dd790c82bb1
zsluss <zluss@gmail.com>
2026-02-11 13:11:51 -0600

    Refactor index.py to calculate and visualize average metrics for champions, non-champions, and top 10 seeds with improved bar chart representation

--- index.py
+++ index.py
@@ -14,7 +14,6 @@ df = df.sort_values(by='YEAR', ascending=False)
 champion_data=  df[df['POSTSEASON'] == 'Champions']
 champion_data['TEAM+YEAR'] = champion_data['TEAM'] + ' ' + champion_data['YEAR'].astype(str)
 #print(champion_data)
-print(champion_data)
 
 
 # Create a horizontal bar chart to visualize the adjusted offensive efficiency (ADJOE) of NCAA champions
@@ -61,43 +60,46 @@ plt.show()
 
 nonChampion_data = df[df['POSTSEASON'] != 'Champions']
 top10seed_data = df[df['SEED'] <= 10]
-champion_avg_ADJOE = champion_data['ADJOE'].mean()
-nonChampion_avg_ADJOE = nonChampion_data['ADJOE'].mean()
-top10seeed_avg_ADJOE = top10seed_data['ADJOE'].mean()
-champion_avg_ADJDE = champion_data['ADJDE'].mean()
-nonChampion_avg_ADJDE = nonChampion_data['ADJDE'].mean()
-top10seed_avg_ADJDE = top10seed_data['ADJDE'].mean()
-champion_avg_EFG0 = champion_data['EFG_O'].mean()
-nonChampion_avg_EFG0 = nonChampion_data['EFG_O'].mean()
-top10seed_avg_EFG0 = top10seed_data['EFG_O'].mean()
-champion_avg_EFGD = champion_data['EFG_D'].mean()
-nonChampion_avg_EFGD = nonChampion_data['EFG_D'].mean()
-top10seed_avg_EFGD = top10seed_data['EFG_D'].mean()
-champion_avg_tempo= champion_data['ADJ_T'].mean()
-nonChampion_avg_tempo = nonChampion_data['ADJ_T'].mean()
-top10seed_avg_tempo = top10seed_data['ADJ_T'].mean()
+champion_avg_ADJOE = champion_data['ADJOE'].mean().round(1)
+nonChampion_avg_ADJOE = nonChampion_data['ADJOE'].mean().round(1)
+top10seeed_avg_ADJOE = top10seed_data['ADJOE'].mean().round(1)
+champion_avg_ADJDE = champion_data['ADJDE'].mean().round(1)
+nonChampion_avg_ADJDE = nonChampion_data['ADJDE'].mean().round(1)
+top10seed_avg_ADJDE = top10seed_data['ADJDE'].mean().round(1)
+champion_avg_EFG0 = champion_data['EFG_O'].mean().round(1)
+nonChampion_avg_EFG0 = nonChampion_data['EFG_O'].mean().round(1)
+top10seed_avg_EFG0 = top10seed_data['EFG_O'].mean().round(1)
+champion_avg_EFGD = champion_data['EFG_D'].mean().round(1)
+nonChampion_avg_EFGD = nonChampion_data['EFG_D'].mean().round(1)
+top10seed_avg_EFGD = top10seed_data['EFG_D'].mean().round(1)
+champion_avg_tempo= champion_data['ADJ_T'].mean().round(1)
+nonChampion_avg_tempo = nonChampion_data['ADJ_T'].mean().round(1)
+top10seed_avg_tempo = top10seed_data['ADJ_T'].mean().round(1)
 
+
+#Chart that compares the average values of ADJOE, ADJDE, EFG_O, EFG_D, and ADJ_T for champions, non-champions, and top 10 seeds
 names = ['ADJOE', 'ADJDE', 'EFG_O', 'EFG_D', 'ADJ_T']
 means = {  
-    'ADJOE': [champion_avg_ADJOE, nonChampion_avg_ADJOE, top10seeed_avg_ADJOE],
-    'ADJDE': [champion_avg_ADJDE, nonChampion_avg_ADJDE, top10seed_avg_ADJDE],
-    'EFG_O': [champion_avg_EFG0, nonChampion_avg_EFG0, top10seed_avg_EFG0],
-    'EFG_D': [champion_avg_EFGD, nonChampion_avg_EFGD, top10seed_avg_EFGD],
-    'ADJ_T': [champion_avg_tempo, nonChampion_avg_tempo, top10seed_avg_tempo]
+
+    'Champions': [champion_avg_ADJOE, champion_avg_ADJDE, champion_avg_EFG0, champion_avg_EFGD, champion_avg_tempo],
+    'Non-Champions': [nonChampion_avg_ADJOE, nonChampion_avg_ADJDE, nonChampion_avg_EFG0, nonChampion_avg_EFGD, nonChampion_avg_tempo],
+    'Top 10 Seeds': [top10seeed_avg_ADJOE, top10seed_avg_ADJDE, top10seed_avg_EFG0, top10seed_avg_EFGD, top10seed_avg_tempo]
 }
-x = np.arange(len(names))
-width = 0.15
+width = 0.5
+group_gap = 0.5
+group_width = width * len(means)
+x = np.arange(len(names)) * (group_width + group_gap)
 multiplier = 0
 
 fig, ax = plt.subplots(layout='constrained')
-for attribute, mean in means.items():
+for attribute,measurement in means.items():
     offset = width * multiplier
-    rects = ax.bar(x + offset, mean, width, label=attribute)
+    rects = ax.bar(x + offset, measurement, width, label=attribute)
     ax.bar_label(rects, padding=3)
     multiplier += 1
 
+ax.set_xticks(x + group_width / 2 - width / 2, names)
+ax.legend(loc='upper right', ncols=3)
 ax.set_ylabel('Average Values')
-ax.set_title('Average Values of Different Attributes for Champions, Non-Champions, and Top 10 Seeds')
-ax.set_xticks(x + width, names)
-ax.legend(loc='upper left', ncols=2)
+ax.set_title('Average Metrics for Champions, Non-Champions, and Top 10 Seeds')
 plt.show()
