3e908a9ae08994d99c9e8e7011e900f433bd1ced
zsluss <zluss@gmail.com>
2026-02-11 11:56:17 -0600

    Add .gitignore and update index.py to load and display champion data

--- index.py
+++ index.py
@@ -2,5 +2,16 @@
 
 # Zach Slusser
 
-# Mini Project 2
+# Mini Project 2 
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load the dataset
+df= pd.read_csv('data/cbb.csv')
+champion_data=  df[df['POSTSEASON'] == 'Champions']
+plottingData = champion_data
+
+#print(champion_data)
+print(champion_data)
