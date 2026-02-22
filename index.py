4e5e2966ea0872d37814ecb41828fe7c4b880caa
zsluss <zluss@gmail.com>
2026-02-22 08:12:06 -0600

    Fix typo in variable name for top 10 seeds average ADJOE calculation in index.py

--- index.py
+++ index.py
@@ -79,7 +79,7 @@ nonChampion_data = df[df['POSTSEASON'] != 'Champions']
 top10seed_data = df[df['SEED'] <= 10]
 champion_avg_ADJOE = champion_data['ADJOE'].mean().round(1)
 nonChampion_avg_ADJOE = nonChampion_data['ADJOE'].mean().round(1)
-top10seeed_avg_ADJOE = top10seed_data['ADJOE'].mean().round(1)
+top10seed_avg_ADJOE = top10seed_data['ADJOE'].mean().round(1)
 champion_avg_ADJDE = champion_data['ADJDE'].mean().round(1)
 nonChampion_avg_ADJDE = nonChampion_data['ADJDE'].mean().round(1)
 top10seed_avg_ADJDE = top10seed_data['ADJDE'].mean().round(1)
@@ -94,13 +94,15 @@ nonChampion_avg_tempo = nonChampion_data['ADJ_T'].mean().round(1)
 top10seed_avg_tempo = top10seed_data['ADJ_T'].mean().round(1)
 
 
+
+
 #Chart that compares the average values of ADJOE, ADJDE, EFG_O, EFG_D, and ADJ_T for champions, non-champions, and top 10 seeds
 names = ['ADJOE', 'ADJDE', 'EFG_O', 'EFG_D', 'ADJ_T']
 means = {  
 
     'Champions': [champion_avg_ADJOE, champion_avg_ADJDE, champion_avg_EFG0, champion_avg_EFGD, champion_avg_tempo],
     'Non-Champions': [nonChampion_avg_ADJOE, nonChampion_avg_ADJDE, nonChampion_avg_EFG0, nonChampion_avg_EFGD, nonChampion_avg_tempo],
-    'Top 10 Seeds': [top10seeed_avg_ADJOE, top10seed_avg_ADJDE, top10seed_avg_EFG0, top10seed_avg_EFGD, top10seed_avg_tempo]
+    'Top 10 Seeds': [top10seed_avg_ADJOE, top10seed_avg_ADJDE, top10seed_avg_EFG0, top10seed_avg_EFGD, top10seed_avg_tempo]
 }
 #This is to get the 3 bars to be groupted together but also have space in between the groups, I had to look up how to do this because using the documentation from matplotlib was causing overlap.
 width = 0.5
